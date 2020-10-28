1bf957be9e805fddae9ccd7ec858126891e99fbf
Ashwin Dev <ashwindev1462001@gmail.com>
2020-10-28 22:18:25 +0530

    After removing pdf

---
+++ ContactUs.php
@@ -0,0 +1,277 @@
+<?php
+	session_start();
+	if(isset($_POST['send'])){
+		if(isset($_POST['username']) && strlen($_POST['username'])>0 ){
+			if( isset($_POST['feedback']) && strlen($_POST['feedback'])>0 ){
+				$_SESSION['success'] = "Thanks for feedback";
+				unset($_SESSION['username']);
+				unset($_SESSION['feedback']);
+			}
+			else{
+				$_SESSION['error'] = "Enter feedback.";
+				$_SESSION['username'] = $_POST['username'];
+			}
+			
+			
+		}
+		else{
+			$_SESSION['error'] = "Enter your name.";
+			if(isset($_POST['feedback']) && strlen($_POST['feedback'])>0){
+				$_SESSION['feedback'] = $_POST['feedback'];
+			}
+			
+		}
+		
+	}
+	
+	if( !isset($_SESSION['username']) ){
+		$_SESSION['username'] = "";
+	}
+	if( !isset($_SESSION['feedback']) ){
+		$_SESSION['feedback'] = "";
+	}
+	
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="description" content="About Us">
+		<meta name="author" content="Ashwin Dev">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+
+		<title>
+			Contact Us
+		</title>
+
+		<link rel = "icon" href ="assets/img/RuralEducationLogo.PNG" type = "image/x-icon">
+
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/css/all.min.css">
+		
+		<link rel="stylesheet" href="assets/css/bootstrap-social.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/board.css">
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		
+		<!--Modal-->
+		<div id="loginModal" class="modal fade" role="dialog">
+			<div class="modal-dialog modal-lg" role="content">
+				<!-- Modal content-->
+				<div class="modal-content">
+					
+					<div class="modal-header">
+							<h4 class="myModalLabel" >INFO</h4>
+						
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+						
+					</div>
+					<div class="modal-body">
+						<div class="row container ">
+							
+								<div class="col-xs-12 col-md-6 ">
+								<?php
+									if( isset($_SESSION['error']) ){
+										echo "<p style='color:red;font-size:25px;'>".$_SESSION['error']."</p>";
+										//unset($_SESSION['error']);
+									}
+								?>
+								</div>
+								<div class="col-xs-12 col-md-6 offset-4">	
+								<?php
+									if( isset($_SESSION['success']) ){
+										echo "<p style='color:green;font-size:25px;'>".$_SESSION['success']."</p>";
+										//unset($_SESSION['success']);
+									}
+								?>
+								</div>
+							
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	
+		<!-- Header -->
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-dark navbar-expand-sm fixed-top navbar-text">
+					<div class="container">
+
+						<!--Logo-->
+						<a class="navbar-brand mr-auto" href="./index.html"><img src="assets/img/RuralEducationLogo.png" height="30" width="61"></a>
+
+
+						<!--NavBar-->
+						<div class="collapse navbar-collapse" id="Navbar">
+							<ul class="navbar-nav  ml-auto">  
+								<li class="nav-item"><a class="nav-link" href="./index.html"><span class="fa fa-home fa-lg"></span> Home</a></li>
+								<li class="nav-item"><a class="nav-link" href="./aboutus.html"><span class="fa fa-info fa-lg"></span> About Us</a></li>
+								<li class="nav-item active"><a class="nav-link" href="#"><span class="fa fa-address-card fa-lg"></span> Contact Us</a></li>
+							</ul>
+							
+						</div>
+						
+						<button class="navbar-toggler btn-sm" type="button" data-toggle="collapse" data-target="#Navbar">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+
+					</div>
+					
+					
+
+					
+					
+				</nav>
+			</div>
+		</header>
+
+		<!-- Main body -->
+		<div class="container mainbody">
+
+
+			<!-- Bread crumbs-->
+			<div class="row row-container mybreadcrumbs"> 
+				<div class="col-12">
+					<ol class="col-12 breadcrumb">
+						<li class="breadcrumb-item"><a href="./index.html">Home</a></li>
+						<li class="breadcrumb-item active">Contact Us</li>
+					</ol>
+				</div>
+			</div>
+
+
+			
+		
+
+			<div class="row row-content">
+			   <div class="col-12">
+				  <h3>Send us your Feedback</h3>
+			   </div>
+			   
+				<!--FeedBack Form-->
+				<div class="col-12 col-md-9">
+					<form method="post" id="FeedBackForm" name="FeedBackForm" >
+						<div class="form-group row">
+							<label for="name" class="col-md-2 col-form-label">Name</label>
+							<div class="col-md-10">
+								<input type="text" class="form-control" id="username" name="username" placeholder="Name" value="<?=$_SESSION['username']?>">
+							</div>
+						</div>
+						
+						<div class="form-group row">
+							<label for="telnum" class="col-12 col-md-2 col-form-label">Contact Tel.</label>
+							<div class="col-12 col-md-10">
+								<input type="tel" class="form-control" id="telnum" name="telnum" placeholder="Tel. number  (optional)">
+							</div>
+						</div>
+						<div class="form-group row">
+							<label for="emailid" class="col-md-2 col-form-label">Email</label>
+							<div class="col-md-10">
+								<input type="email" class="form-control" id="emailid" name="emailid" placeholder="Email  (optional)">
+							</div>
+						</div>
+						<div class="form-group row">
+							<div class="col-md-6 offset-md-2">
+								<div class="form-check">
+									<input type="checkbox" class="form-check-input" name="approve" id="approve" value="" checked>
+									<label class="form-check-label" for="approve">
+										<strong>May we contact you?</strong>
+									</label>
+								</div>
+							</div>
+							
+						</div>
+						<div class="form-group row">
+							<label for="feedback" class="col-md-2 col-form-label">Your Feedback</label>
+							<div class="col-md-10">
+								<textarea class="form-control" id="feedback" name="feedback" rows="12"><?=$_SESSION['feedback']?></textarea>
+							</div>
+						</div>
+						<div class="form-group row">
+							<div class="offset-md-2 col-md-10">
+								<button type="submit" class="btn btn-primary" name="send" id="send">Send Feedback</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+					
+		
+			
+		</div>
+		<!-- Footer -->
+		<footer class="footer">
+			<div class="container">
+				<div class="row">  
+
+					<!--Navigation-->
+					<div class="col-12 offset-1 col-sm-2">
+						<h5>Links</h5>
+						<ul class="list-unstyled">
+							<li><a href="./index.html">Home</a></li>
+							<li><a href="./AboutUs.html">About Us</a></li>
+							<li><a href="#">Contact Us</a></li>
+						</ul>
+					</div>
+					
+
+					<!--Social Media-->
+					<div class="col-12 col-sm-4 align-self-center">
+						<div class="text-center">
+							
+							<a class="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/ashwindev14/" target="_blank"><i class="fa fa-linkedin"></i></a>
+							
+							<a class="btn btn-social-icon" href="mailto:ashwindev1462001@gmail.com"><i class="fa fa-envelope-o"></i></a>
+							<a class="btn btn-social-icon" href="tel:805-615-0426"><i class="fa fa-phone fa-lg" aria-hidden="true" style="font-size:40px;"></i></a>
+							<a href="whatsapp://send?abid=phonenumber&text=Hello%2C%20World!" style="color: #25d366;"><i class="fa fa-whatsapp green-color " aria-hidden="true" style="font-size:40px"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		
+		<div id="testarea"></div>
+		
+		
+		
+		
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/modal.js"></script>
+		<?php
+									if( isset($_SESSION['error']) ){
+										echo "
+										<script>
+											function toggle(){
+												$('#loginModal').modal('show');
+											}
+											toggle() 
+										</script>";
+										unset($_SESSION['error']);
+										
+									}
+									else if( isset($_SESSION['success']) ){
+										echo "
+										<script>
+											function toggle(){
+												$('#loginModal').modal('show');
+											}
+											toggle() 
+										</script>";
+										unset($_SESSION['success']);
+									}
+								?>	
+	</body>
+</html>
---
+++ EnglishMaterial.php
@@ -0,0 +1,298 @@
+<?php
+	if(!isset($_GET['content'])){
+		header("Location: StateBoardEnglish.html");
+		return;
+	}
+	if(!isset($_GET['standard'])){
+		header("Location: StateBoardEnglish.html");
+		return;
+	}
+
+?>
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+	
+		<meta property="og:title" content="Rural Education" />
+		<meta property="og:description" content="Notes and study material for state board and NEET students." />
+		<meta property="og:image" content="assets/img/RuralEducationLogo.PNG" />
+		<meta charset="UTF-8">
+		
+		<meta name="description" content="Rural Education.">
+		<meta name="keywords" content="Study materials, NEET, Tamil Nadu,State Board,Rural Education,Notes,Online Education">
+		<meta name="author" content="Ashwin Dev">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		
+		
+		<title>
+			Rural Education
+		</title>
+		
+		<link rel = "icon" href ="assets/img/RuralEducationLogo.PNG" type = "image/x-icon">
+		
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/css/all.min.css">
+		
+		<link rel="stylesheet" href="assets/css/bootstrap-social.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/board.css">
+	</head>
+	<body>
+
+		<!-- Header -->
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-dark navbar-expand-sm fixed-top navbar-text">
+					<div class="container">
+
+						<!--Logo-->
+						<a class="navbar-brand mr-auto" href="./index.html"><img src="assets/img/RuralEducationLogo.png" height="30" width="61"></a>
+
+
+						<!--NavBar-->
+						<div class="collapse navbar-collapse" id="Navbar">
+							<ul class="navbar-nav  ml-auto">  
+								<li class="nav-item active"><a class="nav-link" href="./index.html"><span class="fa fa-home fa-lg"></span> Home</a></li>
+								<li class="nav-item"><a class="nav-link" href="./aboutus.html"><span class="fa fa-info fa-lg"></span> About Us</a></li>
+								<li class="nav-item"><a class="nav-link" href="./contactus.php"><span class="fa fa-address-card fa-lg"></span> Contact Us</a></li>
+							</ul>
+							
+						</div>
+						
+						<button class="navbar-toggler btn-sm" type="button" data-toggle="collapse" data-target="#Navbar">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+
+					</div>
+					
+					
+
+					
+					
+				</nav>
+			</div>
+		</header>
+
+		<!-- Main body -->
+		<div class="container mainbody">
+
+
+			<!-- Bread crumbs-->
+			<div class="row row-container mybreadcrumbs"> 
+				<div class="col-12">
+					<ol class="col-12 breadcrumb">
+						<li class="breadcrumb-item"><a href="./index.html">Home</a></li>
+						<li class="breadcrumb-item"><a href="./Board.html">Board</a></li>
+						<li class="breadcrumb-item"><a href="./StateBoard.html">State Board</a></li>
+						<li class="breadcrumb-item"><a href="./StateBoardEnglish.html">English</a></li>
+						<li class="breadcrumb-item"><a href="./StateBoardEnglish<?=$_GET['content']?>.html"><?=$_GET['content']?></a></li>
+						<li class="breadcrumb-item active">Standard <?=$_GET['standard']?></li>
+					</ol>
+				</div>
+			</div>
+
+
+			<!--Board selection-->
+			<div class="row justify-content-md-center">
+				<?php
+					
+						$valid = 1;
+						if( $_GET['content'] == "StudyMaterial"){
+							switch($_GET['standard']) {
+								case 1:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard1/";
+									break;
+								case 2:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard2/";
+									break;
+								case 3:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard3/";
+									break;
+								case 4:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard4/";
+									break;
+								case 5:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard5/";
+									break;
+								case 6:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard6/";
+									break;
+								case 7:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard7/";
+									break;
+								case 8:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard8/";
+									break;
+								case 9:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard9/";
+									break;
+								case 10:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard10/";
+									break;
+								case 11:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard11/";
+									break;
+								case 12:
+									$path="assets/pdf/StateBoard/English/StudyMaterial/Standard12/";
+									break;
+								default:
+									echo "
+										<li class='list-group-item list-group-item-action standardlist col-xs-12 col-md-9 col-lg-8'>
+											<div class='col-xs-12 PDFnames' style='color:red;'>Invalid Standard Parameter.Contact the server for help.</div>
+										</li>
+									";
+									$valid=0;
+							}
+							
+							
+						}
+						else if( $_GET['content'] == "QuestionPapers" ){
+							
+							switch($_GET['standard']) {
+								case 1:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard1/";
+									break;
+								case 2:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard2/";
+									break;
+								case 3:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard3/";
+									break;
+								case 4:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard4/";
+									break;
+								case 5:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard5/";
+									break;
+								case 6:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard6/";
+									break;
+								case 7:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard7/";
+									break;
+								case 8:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard8/";
+									break;
+								case 9:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard9/";
+									break;
+								case 10:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard10/";
+									break;
+								case 11:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard11/";
+									break;
+								case 12:
+									$path="assets/pdf/StateBoard/English/QuestionPapers/Standard12/";
+									break;
+								default:
+									echo "
+										<li class='list-group-item list-group-item-action standardlist col-xs-12 col-md-9 col-lg-8'>
+											<div class='col-xs-12 PDFnames' style='color:red;'>Invalid Standard Parameter.Contact the server for help.</div>
+										</li>
+									";
+									$valid = 0;
+							}
+						}
+						else{
+							echo "
+								<li class='list-group-item list-group-item-action standardlist col-xs-12 col-md-9 col-lg-8'>
+									<div class='col-xs-12 PDFnames' style='color:red;'>Invalid Content Parameters.Contact the server for help.</div>
+								</li>
+							";
+							$valid=0;
+						}
+						
+						if($valid == 1){
+							
+							$myDirectory = opendir($path);
+							while($entryName = readdir($myDirectory)) {
+								$dirArray[] = $entryName;
+							}
+							closedir($myDirectory);
+							
+							
+							$indexCount	= count($dirArray);
+							if($indexCount == 2){
+								echo "
+										<li class='list-group-item list-group-item-action standardlist col-xs-12 col-md-9 col-lg-8'>
+											<div class='col-xs-12 PDFnames'>No Content Available</div>
+										</li>
+									";
+							}
+							else{
+								echo "<ul class='col-xs-12 col-md-8'>";
+								for($index=0; $index < $indexCount; $index++) {
+									$extension = substr($dirArray[$index], -3);
+									if ($extension == 'pdf'){ // list only jpgs
+										echo "
+											<li class='list-group-item list-group-item-action standardlist'>
+												<div class='col-xs-12 PDFnames'>".substr($dirArray[$index],0,-4)."</div>
+												<div class='pdfAction col-xs-12'>
+													<a href='".$path.$dirArray[$index]."' target='_blank'>View</a> |
+													<a href='download.php?file=".$path.$dirArray[$index]."'>Download</a>
+												</div>
+											</li>
+										";
+									}	
+								}
+								echo "</ul>";
+							}
+							unset($valid);
+						}
+					
+					?>
+
+			</div>
+
+
+		</div>
+		
+			
+  
+		<!-- Footer -->
+		<footer class="footer">
+			<div class="container">
+				<div class="row">  
+
+					<!--Navigation-->
+					<div class="col-12 offset-1 col-sm-2">
+						<h5>Links</h5>
+						<ul class="list-unstyled">
+							<li><a href="./index.html">Home</a></li>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Contact Us</a></li>
+						</ul>
+					</div>
+					
+
+					<!--Social Media-->
+					<div class="col-12 col-sm-4 align-self-center">
+						<div class="text-center">
+							
+							<a class="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/ashwindev14/" target="_blank"><i class="fa fa-linkedin"></i></a>
+							
+							<a class="btn btn-social-icon" href="mailto:ashwindev1462001@gmail.com"><i class="fa fa-envelope-o"></i></a>
+							<a class="btn btn-social-icon" href="tel:805-615-0426"><i class="fa fa-phone fa-lg" aria-hidden="true" style="font-size:40px;"></i></a>
+							<a href="whatsapp://send?abid=phonenumber&text=Hello%2C%20World!" style="color: #25d366;"><i class="fa fa-whatsapp green-color " aria-hidden="true" style="font-size:40px"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		
+		
+		
+		
+		
+		
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+	</body>
+</html>
