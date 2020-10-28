ae11cf58425f966ffcc4b1146a689988c235b5e9
Ashwin Dev <ashwindev1462001@gmail.com>
2020-10-28 22:19:35 +0530

    ignoring only pdf

---
+++ assets/js/modal.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	
+	/*$('#FeedBackForm').on('submit', function(e){
+		
+		$('#loginModal').modal('show');
+		e.preventDefault();
+	});
+	*/
+	
+	
+});
+function toggle(){
+	$('#loginModal').modal('show');
+}
+
+
+
+
