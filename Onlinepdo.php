63f40a3c580b20cb4156b63d502fd13a2feb6dab
Ashwin Dev <ashwindev1462001@gmail.com>
2020-11-02 23:27:09 +0530

    Before hosting after making the pdo ready

---
+++ Onlinepdo.php
@@ -0,0 +1,3 @@
+<?php
+$pdo = new PDO('mysql:host=	sql113.epizy.com;port=3306;dbname=epiz_27076705_educationforeveryone', "epiz_27076705", 'YNEGDOfcQ9fQ3pE');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
